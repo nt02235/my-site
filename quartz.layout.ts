c1a8dd48c75ddbf25e8c2b94b72243a534ca98f7
nt02235 <nathalie.torres@gmail.com>
2026-05-19 23:57:43 -0400

    Quartz sync: May 19, 2026, 11:57 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,16 +28,16 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     // Component.PageTitle(),
     // Component.MobileOnly(Component.Spacer()),
-    // Component.Flex({
-    //   components: [
-    //     {
-    //       Component: Component.Search(),
-    //       grow: true,
-    //     },
-    //     { Component: Component.Darkmode() },
-    //     { Component: Component.ReaderMode() },
-    //   ],
-    // }),
+    Component.Flex({
+      components: [
+        // {
+        //   Component: Component.Search(),
+        //   grow: true,
+        // },
+        { Component: Component.Darkmode() }
+        // { Component: Component.ReaderMode() },
+      ],
+    }),
     // Component.Explorer(),
   ],
   right: [
